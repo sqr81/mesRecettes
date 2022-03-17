05a33115fc2a849d295bc13f27c973cce241c13d
Yann <yann.sanquer@epitech.fr>
2022-03-17 16:41:52 +0100

    navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import RecipesList from '../screens/RecipesList';
-import RecipesDetails from '../screens/RecipesDetails';
 import TasksScreen from '../screens/Tasks';
-
-const Stack = createNativeStackNavigator();
+import {RecipesStack} from './RecipesStack';
 const Tabs = createBottomTabNavigator();
-
-export const RecipesStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="RecipesList" component={RecipesList} />
-      <Stack.Screen name="RecipesDetails" component={RecipesDetails} />
-    </Stack.Navigator>
-  );
-};
 
 export default function AppNavigation() {
   return (
@@ -25,6 +12,7 @@ export default function AppNavigation() {
       <Tabs.Navigator screenOptions={{headerShown: false}}>
         <Tabs.Screen name="Recettes" component={RecipesStack} />
         <Tabs.Screen name="Liste" component={TasksScreen} />
+        <Tabs.Screen name="Profile" component={TasksScreen} />
       </Tabs.Navigator>
       {/*<Stack.Navigator>
         <Stack.Screen name="TasksList" component={TasksScreen} />
